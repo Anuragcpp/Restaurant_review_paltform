323e49288070375773d1a8857a8dbd8368dad846
Anurag Sen <anuragsen@Anurags-MacBook-Pro.local>
2025-12-05 22:28:57 +0530

    inital commit

---
+++ src/test/java/com/project/Restourent/RestaurentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Restourent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
